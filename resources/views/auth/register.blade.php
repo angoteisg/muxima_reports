941032c87b13ac4a4b582099a85413c7c181a2a7
DiogoManuel <diogomanuel08@gmail.com>
2022-04-28 09:45:54 +0100

    Login e Register
    Mudanças nas cores das label do Login, Registo, logo Muxima Reports.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,31 +1,31 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+@extends('layouts.guest')
+@section('content')
 
-        <x-jet-validation-errors class="mb-4" />
-
-        <form method="POST" action="{{ route('register') }}">
+<div class="autenticacao" style=" ">
+    <center> <img  src="{{ asset('imagens/MuximaReports.png') }}" style="width:70%; height:auto" alt=""></center>
+     
+    <br>
+    <br>
+        <form method="POST" class="formAuth" action="{{ route('register') }}">
             @csrf
 
             <div>
-                <x-jet-label for="name" value="{{ __('Name') }}" />
+                <b> <x-jet-label for="name" value="{{ __('Nome') }}"  /></b>
                 <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <strong><x-jet-label for="email" value="{{ __('Email') }}" style="color: #000000 !important; font-weight: bold;"/></strong>
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-label for="password" value="{{ __('Senha') }}" style="color: #547c8e !important; font-weight: bold;"/>
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                <x-jet-label for="password_confirmation" value="{{ __('Confirme a Senha') }}" style="color: #8c1014 !important; font-weight: bold;"/>
                 <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
 
@@ -47,14 +47,16 @@
             @endif
 
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}" style="color: #8c1014 !important; font-weight: bold;">
                     {{ __('Already registered?') }}
                 </a>
 
-                <x-jet-button class="ml-4">
-                    {{ __('Register') }}
+                <x-jet-button class="ml-4" style="color: #ffffff !important; font-weight: bold;">
+                    {{ __('Registar') }}
                 </x-jet-button>
             </div>
         </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+    </div>
+
+
+@endsection
